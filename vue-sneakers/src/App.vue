0163db012515f4084ea5667405162fce6028a96f
daniilcyplakov <daniil_fox77@mail.ru>
2023-12-10 14:28:13 +0400

    header ready 1h 38m

--- vue-sneakers/src/App.vue
+++ vue-sneakers/src/App.vue
@@ -1,47 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="bg-white rounded-xl shadow-xl mt-14 h-screen w-4/5 m-auto">
+    <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+      <div class="flex items-center gap-4">
+         <img src="/logo.png" alt="Logo" class="w-10">
+         <div>
+          <h2 class="text-xl font-bold uppercase">Vue sneakers</h2>
+          <p class="text-slate-400">Магазин лучших кросовок</p>
+         </div>
+      </div>
+      <ul class="flex items-center gap-10">
+        <li class="flex items-center gap-3">
+          <img src="/cart.svg" alt="">
+          <b>1205 руб</b>
+        </li>
+        <li class="flex items-center gap-3">
+          <img src="/heart.svg" alt="">
+          <b>Закладки</b>
+        </li>
+        <li class="flex items-center gap-3">
+          <img src="/profile.svg" alt="">
+          <b>Профиль</b>
+        </li>
+      </ul>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    </header>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
